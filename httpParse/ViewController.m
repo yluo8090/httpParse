e78390db6aa2f8359cabe4a6d418c7d4451721c7
luoyao <luoyao@zen-phone.com>
2017-07-19 11:35:03 +0800

    更新了，实际通过get请求去登录获取HTML源码，然后解析。

--- httpParse/ViewController.m
+++ httpParse/ViewController.m
@@ -7,7 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "AFNetworking.h"
 #import "TFHpple.h"
+
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <net/if.h>
+#include <ifaddrs.h>
+#import <dlfcn.h>
 
 @interface ViewController ()
 
@@ -17,30 +24,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self netWork];
+    
+    NSLog(@"localAddress:%@",[self localWiFiIPAddress]);
+}
+
+
+- (void)netWork{
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    //设置非校验证书模式
+    manager.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModeNone];
+    manager.securityPolicy.allowInvalidCertificates = YES;
+    [manager.securityPolicy setValidatesDomainName:NO];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    
+    [manager GET:@"https://192.168.1.1/cgi-bin/luci?luci_username=root&luci_password=admin" parameters:nil progress:nil success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSString *html = [[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding];
+        if (html) {
+            [self parseHtml:html];
+        }
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        NSLog(@"%@",error.description);
+    }];
+}
+
+- (NSString *)parseHtml:(NSString *)htmlStr{
     
     NSString *rssiStr = nil;
-    
-    NSString *htmlPath = [[NSBundle mainBundle] pathForResource:@"RSSI" ofType:@"html"];
-    NSData *htmlData = [NSData dataWithContentsOfFile:htmlPath];
-    
+    NSData *htmlData = [htmlStr dataUsingEncoding:NSUTF8StringEncoding];
+
     TFHpple *doc = [[TFHpple alloc] initWithHTMLData:htmlData];
     NSArray *elements = [doc searchWithXPathQuery:@"//fieldset[@class='cbi-section']"];
-    
-    
-//    TFHppleElement *element = elements[2];
-//    
-//    NSArray *els = [element children];
-//    TFHppleElement *el = els[3];
-//    
-//    NSArray *es = [el children];
-//    
-//    TFHppleElement *e = es[9];
-//    
-//    NSArray *ls = [e children];
-//    TFHppleElement *l = ls.lastObject;
-//    
-//     NSLog(@"html:%@",[l text]);
-    
     
     for (TFHppleElement *elment in elements) {
         for (TFHppleElement *el in [elment children]) {
@@ -51,7 +67,7 @@
                     
                     NSArray *tables = [table children];
                     for (TFHppleElement *tr in tables) {
-
+                        
                         if ([[tr text] isEqualToString:@"RSSI"]) {
                             NSUInteger index = [tables indexOfObject:tr];
                             
@@ -63,12 +79,34 @@
             }
         }
     }
-    
-    NSLog(@"RSSI:%@",rssiStr);
-    
+    NSLog(@"success:%@",[rssiStr stringByReplacingOccurrencesOfString:@"" withString:@" "]);
+    return rssiStr;
 }
 
 
+//获取本地ip
+- (NSString *) localWiFiIPAddress
+{
+    BOOL success;
+    struct ifaddrs * addrs;
+    const struct ifaddrs * cursor;
+    success = getifaddrs(&addrs) == 0;
+    if (success) {
+        cursor = addrs;
+        while (cursor != NULL) {
+            // the second test keeps from picking up the loopback address
+            if (cursor->ifa_addr->sa_family == AF_INET && (cursor->ifa_flags & IFF_LOOPBACK) == 0)
+            {
+                NSString *name = [NSString stringWithUTF8String:cursor->ifa_name];
+                if ([name isEqualToString:@"en0"])  // Wi-Fi adapter
+                    return [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)cursor->ifa_addr)->sin_addr)];
+            }
+            cursor = cursor->ifa_next;
+        }
+        freeifaddrs(addrs);
+    }
+    return nil;
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
