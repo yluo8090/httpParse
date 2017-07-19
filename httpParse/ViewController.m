9ef242e36c2734c2d53bdc7fb5d487ed3d8b409e
luoyao <luoyao@zen-phone.com>
2017-07-19 17:00:57 +0800

    加入获取本地IP和网关地址方法

--- httpParse/ViewController.m
+++ httpParse/ViewController.m
@@ -10,11 +10,14 @@
 #import "AFNetworking.h"
 #import "TFHpple.h"
 
-#include <arpa/inet.h>
-#include <netdb.h>
-#include <net/if.h>
-#include <ifaddrs.h>
+#import <netdb.h>
+#import <net/if.h>
+
 #import <dlfcn.h>
+#import <arpa/inet.h>
+#import <netinet/in.h>
+#import <ifaddrs.h>
+#import "getgateway.h"
 
 @interface ViewController ()
 
@@ -26,7 +29,7 @@
     [super viewDidLoad];
     [self netWork];
     
-    NSLog(@"localAddress:%@",[self localWiFiIPAddress]);
+    NSLog(@"localAddress:%@",[self getGatewayIpForCurrentWiFi]);
 }
 
 
@@ -107,6 +110,57 @@
     }
     return nil;
 }
+
+//获取网关
+- (NSString *)getGatewayIpForCurrentWiFi {
+    NSString *address = @"error";
+    struct ifaddrs *interfaces = NULL;
+    struct ifaddrs *temp_addr = NULL;
+    int success = 0;
+    // retrieve the current interfaces - returns 0 on success
+    success = getifaddrs(&interfaces);
+    if (success == 0) {
+        // Loop through linked list of interfaces
+        temp_addr = interfaces;
+        //*/
+        while(temp_addr != NULL) {
+            /*/
+             int i=255;
+             while((i--)>0)
+             //*/
+            if(temp_addr->ifa_addr->sa_family == AF_INET) {
+                // Check if interface is en0 which is the wifi connection on the iPhone
+                if([[NSString stringWithUTF8String:temp_addr->ifa_name] isEqualToString:@"en0"])
+                {
+                    // Get NSString from C String //ifa_addr
+                    //ifa->ifa_dstaddr is the broadcast address, which explains the "255's"
+                    //                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)];
+                    address = [NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)];
+                    /*
+                    //routerIP----192.168.1.255 广播地址
+                    NSLog(@"broadcast address--%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_dstaddr)->sin_addr)]);
+                    //--192.168.1.106 本机地址
+                    NSLog(@"local device ip--%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_addr)->sin_addr)]);
+                    //--255.255.255.0 子网掩码地址
+                    NSLog(@"netmask--%@",[NSString stringWithUTF8String:inet_ntoa(((struct sockaddr_in *)temp_addr->ifa_netmask)->sin_addr)]);
+                    //--en0 端口地址
+                    NSLog(@"interface--%@",[NSString stringWithUTF8String:temp_addr->ifa_name]);
+                     */
+                }
+            }
+            temp_addr = temp_addr->ifa_next;
+        }
+    }
+    // Free memory
+    freeifaddrs(interfaces);
+    in_addr_t i = inet_addr([address cStringUsingEncoding:NSUTF8StringEncoding]);
+    in_addr_t* x = &i;
+    unsigned char *s = getdefaultgateway(x);
+    NSString *ip=[NSString stringWithFormat:@"%d.%d.%d.%d",s[0],s[1],s[2],s[3]];
+    free(s);
+    return ip;
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
